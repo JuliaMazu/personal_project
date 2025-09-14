46ed08e4530121870902f558bf7db5e9ab2a5bf9
Mazur Iuliia <yuliya.vorona@gmail.com>
2025-09-14 17:27:00 +0200

    applicaiton

---
+++ app.py
@@ -0,0 +1,13 @@
+from dash import Dash, html
+
+# Initialize the Dash app
+app = Dash(__name__)
+
+# Define the layout of the app
+app.layout = html.Div(children=[
+    html.H1(children='Hello User')
+])
+
+# Run the app
+if __name__ == '__main__':
+    app.run_server(debug=True)
