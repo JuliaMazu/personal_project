03f1835a80bb716d55c76ed9a43c72500eaf83e2
Mazur Iuliia <yuliya.vorona@gmail.com>
2025-10-08 09:59:17 +0200

    changes in flask headers

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from dash import Dash, html
-from fastapi.middleware.cors import CORSMiddleware
+from flask import Flask, request, jsonify
 import os
 import uuid
 import pandas as pd
@@ -26,13 +26,28 @@ origins = [
     'http://localhost:8050'          # autorise les tests locaux
 ]
 
-server.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_methods=['*'],
-    allow_headers=['*'],
-    max_age=3600
-)
+@server.after_request
+def add_cors_headers(response):
+    # Determine the origin of the request
+    origin = request.headers.get('Origin')
+    
+    # Check if the origin is allowed
+    if origin in origins:
+        response.headers['Access-Control-Allow-Origin'] = origin
+        response.headers['Access-Control-Allow-Headers'] = '*'
+        response.headers['Access-Control-Allow-Methods'] = '*'
+        response.headers['Access-Control-Max-Age'] = '3600'
+    
+    # Handle preflight (OPTIONS) requests
+    if request.method == 'OPTIONS':
+        response = server.response_class()
+        response.headers['Access-Control-Allow-Origin'] = origin
+        response.headers['Access-Control-Allow-Headers'] = '*'
+        response.headers['Access-Control-Allow-Methods'] = '*'
+        response.headers['Access-Control-Max-Age'] = '3600'
+        return response
+        
+    return response
 
                 ###layout
 app.layout = dbc.Container([
